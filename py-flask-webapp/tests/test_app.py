214c229b97dfdafe336d23978631c3877f8be55f
Ubuntu <adminuser@assignment3-vm.sh1vcsqlfm0ejmxas2eqadt5jf.bx.internal.cloudapp.net>
2025-06-17 19:27:18 +0000

    updated pytest file and Jenkins file

--- py-flask-webapp/tests/test_app.py
+++ py-flask-webapp/tests/test_app.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
-def test_home_route():
+def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
