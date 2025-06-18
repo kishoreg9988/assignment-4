2ec774689eb138aa403413a0ac3c5809e30dd31c
Ubuntu <adminuser@assignment3-vm.sh1vcsqlfm0ejmxas2eqadt5jf.bx.internal.cloudapp.net>
2025-06-18 07:08:27 +0000

    udated Jenkins file for testing

--- py-flask-webapp/app.py
+++ py-flask-webapp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask in Docker with pytest..Thanks!"
+    return "Hello from Flask in Docker with pytest..testing!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
